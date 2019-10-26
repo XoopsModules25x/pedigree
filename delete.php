6717fa6c1d810781b738be55c8f409c42fa9a866
zyspec <owners@zyspec.com>
2019-10-26 18:57:39 -0500

    Refactor ./delete.php & ./deletepage.php
    * use Xmf\Request
    * use Pedigree\Helper methods
    * refactor to use class/methods instead of direct SQL access

--- delete.php
+++ delete.php
@@ -1,65 +1,76 @@
 <?php
-// -------------------------------------------------------------------------
+/*
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
 
-//require_once  dirname(dirname(__DIR__)) . '/mainfile.php';
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/**
+ * Module: Pedigree
+ *
+ * @package   XoopsModules\Pedigree
+ * @copyright Copyright (c) 2001-2019 {@link https://xoops.org XOOPS Project}
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @author    XOOPS Module Development Team
+ */
+
+use Xmf\Request;
+use XoopsModules\Pedigree;
+use XoopsModules\Pedigree\Constants;
+
 require_once __DIR__ . '/header.php';
-$moduleDirName = basename(__DIR__);
-xoops_loadLanguage('main', $moduleDirName);
+
+/** @var XoopsModules\Pedigree\Helper $helper */
+$helper->loadLanguage('main');
+
 // Include any common code for this module.
-require_once XOOPS_ROOT_PATH . '/modules/' . $moduleDirName . '/include/common.php';
-
-$GLOBALS['xoopsOption']['template_main'] = 'pedigree_delete.tpl';
-
-include XOOPS_ROOT_PATH . '/header.php';
-
-//get module configuration
-/** @var XoopsModuleHandler $moduleHandler */
-$moduleHandler = xoops_getHandler('module');
-$module = $moduleHandler->getByDirname($moduleDirName);
-$configHandler = xoops_getHandler('config');
-$moduleConfig = $configHandler->getConfigsByCat(0, $module->getVar('mid'));
+require_once $helper->path('include/common.php');
 
 //check for access
-$xoopsModule = XoopsModule::getByDirname($moduleDirName);
 if (empty($GLOBALS['xoopsUser']) || !($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
     redirect_header('javascript:history.go(-1)', 3, _NOPERM . '<br>' . _MA_PEDIGREE_REGIST);
 }
 
-global $xoopsTpl;
-global $xoopsDB;
-global $xoopsModuleConfig;
+$GLOBALS['xoopsOption']['template_main'] = 'pedigree_delete.tpl';
+include XOOPS_ROOT_PATH . '/header.php';
 
-$id = $_GET['id'];
-//query (find values for this dog (and format them))
-$queryString = 'SELECT naam, user, roft FROM ' . $GLOBALS['xoopsDB']->prefix('pedigree_tree') . ' WHERE id=' . $id;
-$result = $GLOBALS['xoopsDB']->query($queryString);
-
-while (false !== ($row = $GLOBALS['xoopsDB']->fetchArray($result))) {
-    //ID
-    $id = $row['id'];
-    //name
-    $naam = htmlentities(stripslashes($row['naam']), ENT_QUOTES);
-    $namelink = '<a href="dog.php?id=' . $row['id'] . '">' . stripslashes($row['naam']) . '</a>';
-    //user who entered the info
-    $dbuser = $row['user'];
-    $roft = $row['roft'];
+$id = Request::getInt('id', 0, 'GET');
+if (empty($id)) {
+    //re-route user if we didn't get a valid id
+    $helper->redirect('', Constants::REDIRECT_DELAY_MEDIUM, _MA_PEDIGREE_INVALID_ID);
 }
+//query - find values for this animal
+$treeHandler = $helper->getHandler('Tree');
+$treeObj = $treeHandler->get($id);
 
-//create form
-include XOOPS_ROOT_PATH . '/class/xoopsformloader.php';
-$form = new \XoopsThemeForm($naam, 'deletedata', 'deletepage.php', 'post', true);
-//hidden value current record owner
-$form->addElement(new \XoopsFormHidden('dbuser', $dbuser));
-//hidden value dog ID
-$form->addElement(new \XoopsFormHidden('dogid', $_GET['id']));
-$form->addElement(new \XoopsFormHidden('curname', $naam));
-$form->addElement(new \XoopsFormHiddenToken($name = 'XOOPS_TOKEN_REQUEST', $timeout = 360));
-$form->addElement(new \XoopsFormLabel(_MA_PEDIGREE_DELE_SURE, _MA_PEDIGREE_DEL_MSG . $moduleConfig['animalType'] . ' : <b>' . $naam . '</b>?'));
-$pups = pups($_GET['id'], $roft);
-$form->addElement(new \XoopsFormLabel(_MA_PEDIGREE_DELE_WARN, _MA_PEDIGREE_ALL . $moduleConfig['children'] . _MA_PEDIGREE_ALL_ORPH . $pups));
-$form->addElement(new \XoopsFormButton('', 'button_id', _DELETE, 'submit'));
-//add data (form) to smarty template
-$xoopsTpl->assign('form', $form->render());
+if ($treeObj instanceof Pedigree\Tree) {
+    $naam = $treeObj->getVar('naam', 's');
+    //$namelink = "<a href=\"" . $helper->url("dog.php?id={$id}") . "\">{$naam}</a>";
+
+    //Create form
+    include XOOPS_ROOT_PATH . '/class/xoopsformloader.php';
+    $form = new \XoopsThemeForm($naam, 'deletedata', 'deletepage.php', 'post');
+    //hidden value current record owner
+    $form->addElement(new \XoopsFormHidden('dbuser', $treeObj->getVar('user')));
+    //hidden value dog ID
+    $form->addElement(new \XoopsFormHidden('dogid', $id));
+    $form->addElement(new \XoopsFormHidden('curname', $naam));
+    $form->addElement(new \XoopsFormHiddenToken('XOOPS_TOKEN_REQUEST', Constants::TOKEN_TIMEOUT));
+    $form->addElement(new \XoopsFormLabel(_MA_PEDIGREE_DELE_SURE, _MA_PEDIGREE_DEL_MSG . $helper->getConfig['animalType'] . " : <span style=\"font-weight: bold;\">{$naam}</span>?"));
+    //@todo move pups() function to Tree class method
+    $pups = pups($id, (int)$treeObj->getVar('roft'));
+    $form->addElement(new \XoopsFormLabel(_MA_PEDIGREE_DELE_WARN, _MA_PEDIGREE_ALL . $helper->getConfig['children'] . _MA_PEDIGREE_ALL_ORPH . $pups));
+    $form->addElement(new \XoopsFormButton('', 'button_id', _DELETE, 'submit'));
+    //add data (form) to smarty template
+    $GLOBALS['xoopsTpl']->assign('form', $form->render());
+} else {
+    //redirect because this animal wasn't found
+    $helper->redirect('', Constants::REDIRECT_DELAY_MEDIUM, _MA_PEDIGREE_INVALID_ID);
+}
 
 //footer
 include XOOPS_ROOT_PATH . '/footer.php';
--- deletepage.php
+++ deletepage.php
@@ -1,46 +1,80 @@
 <?php
-// -------------------------------------------------------------------------
+/*
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/**
+ * Module: Pedigree
+ *
+ * @package   \XoopsModules\Pedigree
+ * @copyright Copyright (c) 2001-2019 {@link https://xoops.org XOOPS Project}
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @author    XOOPS Module Development Team
+ */
 
 use Xmf\Request;
+use XoopsModules\Pedigree;
+use XoopsModules\Pedigree\Constants;
 
-//require_once  dirname(dirname(__DIR__)) . '/mainfile.php';
 require_once __DIR__ . '/header.php';
-$moduleDirName = basename(__DIR__);
-xoops_loadLanguage('main', $moduleDirName);
+/** @var XoopsModules\Pedigree\Helper $helper */
+$helper->loadLanguage('main');
+
 // Include any common code for this module.
-require_once XOOPS_ROOT_PATH . '/modules/' . $moduleDirName . '/include/common.php';
+require_once $helper->path('include/common.php');
 
 $GLOBALS['xoopsOption']['template_main'] = 'pedigree_delete.tpl';
-
 include XOOPS_ROOT_PATH . '/header.php';
 
-//check for access
-$xoopsModule = XoopsModule::getByDirname($moduleDirName);
+//check XOOPS security token
+if (!$GLOBALS['xoopsSecurity']->check()) {
+    $helper->redirect('', Constants::REDIRECT_DELAY_MEDIUM, implode('<br>', $GLOBALS['xoopsSecurity']->getErrors()));
+}
+
+//check for authorized user access
 if (empty($GLOBALS['xoopsUser']) || !($GLOBALS['xoopsUser'] instanceof \XoopsUser)) {
     redirect_header('javascript:history.go(-1)', 3, _NOPERM . '<br>' . _MA_PEDIGREE_REGIST);
 }
-
-global $xoopsTpl, $xoopsDB, $xoopsUser;
 
 $dogid = Request::getInt('dogid', 0, 'post');
 $dogname = Request::getString('curname', '', 'post');
 
 if (!empty($dogname)) {
+    $treeHandler = $helper->getHandler('Tree');
+    $treeObj = $treeHandler->get($dogid);
+    if ($treeObj instanceof Pedigree\Tree && !$treeObj->isNew()) {
+        $treeValues = $treeObj->getValues();
+        $trashHandler = $helper->getHandler('Trash');
+        $trashObj = $trashHandler->create();
+        $trashObj->setVars($treeValues);
+        $trashHandler->insert($trashObj);
+        $treeHandler->delete($treeObj);
+        if (Constants::MALE == $treeValues['roft']) {
+            $criteria = new \Criteria('father', $dogid);
+            $treeHandler->updateAll('father', 0, $criteria);
+        } else {
+            $criteria = new \Criteria('mother', $dogid);
+            $treeHandler->updateAll('mother', 0, $criteria);
+        }
+        $helper->redirect('index.php', Constants::REDIRECT_DELAY_MEDIUM, _MD_DATACHANGED);
+    } else {
+        //@todo display more descriptive error message
+        $helper->redirect("dog.php?id={$dogid}", Constants::REDIRECT_DELAY_SHORT, 'ERROR!!');
+    }
+    /*
     $queryString = 'SELECT * FROM ' . $GLOBALS['xoopsDB']->prefix('pedigree_tree') . ' WHERE id=' . $dogid;
     $result = $GLOBALS['xoopsDB']->query($queryString);
     while (false !== ($row = $GLOBALS['xoopsDB']->fetchArray($result))) {
         //check for edit rights
         $access = 0;
         $xoopsModule = XoopsModule::getByDirname($moduleDirName);
-        if (!empty($xoopsUser)) {
-            if ($xoopsUser->isAdmin($xoopsModule->mid())) {
-                $access = 1;
-            }
-            if ($row['user'] == $xoopsUser->getVar('uid')) {
-                $access = 1;
-            }
-        }
-        if ('1' == $access) {
+        if ($helper->isUserAdmin() || $row['user'] == $GLOBALS['xoopsUser']->getVar('uid')) {
             $sql = 'INSERT INTO ' . $GLOBALS['xoopsDB']->prefix('pedigree_trash') . ' SELECT * FROM ' . $GLOBALS['xoopsDB']->prefix('pedigree_tree') . ' WHERE ' . $GLOBALS['xoopsDB']->prefix('pedigree_tree') . ".id='" . $dogid . "'";
             $GLOBALS['xoopsDB']->query($sql);
             $delsql = 'DELETE FROM ' . $GLOBALS['xoopsDB']->prefix('pedigree_tree') . " WHERE id ='" . $row['id'] . "'";
@@ -51,15 +85,12 @@ if (!empty($dogname)) {
                 $sql = 'UPDATE ' . $GLOBALS['xoopsDB']->prefix('pedigree_tree') . " SET mother = '0' where mother = '" . $row['id'] . "'";
             }
             $GLOBALS['xoopsDB']->query($sql);
-            $ch = 1;
+            $changed = true;
         }
     }
-}
-
-if ($ch) {
-    redirect_header('index.php', 2, _MD_DATACHANGED);
+    */
 } else {
-    redirect_header('dog.php?id=' . $dogid, 1, 'ERROR!!');
+    //@todo dog name was empty so send user back to try again
 }
 //footer
 include XOOPS_ROOT_PATH . '/footer.php';
