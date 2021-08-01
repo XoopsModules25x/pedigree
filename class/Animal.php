5bddb2288ee706adda76a5fa9c1d61f5a6309ead
zyspec <zyspec@yahoo.com>
2021-08-01 15:38:34 -0500

    Add Helper and handlers to Animal class
    minor PHPDoc updates
    fix bug in getFormFieldsElements() method retrieving the fieldtype

--- class/Animal.php
+++ class/Animal.php
@@ -15,14 +15,18 @@ namespace XoopsModules\Pedigree;
  * Pedigree module for XOOPS
  *
  * @package     XoopsModules\Pedigree
- * @copyright   {@link http://sourceforge.net/projects/xoops/ The XOOPS Project}
- * @license     {@link http://www.fsf.org/copyleft/gpl.html GNU public license}
+ * @copyright   {@link https://github.com/Xoops The XOOPS Project}
+ * @license     {@link https://www.fsf.org/copyleft/gpl.html GNU public license}
  * @since       1.3.1
  * @author      XOOPS Module Dev Team
  * @author      ZySpec <zyspec@yahoo.com>
  */
-use XoopsModules\Pedigree;
-use XoopsModules\Pedigree\Constants;
+use XoopsModules\Pedigree\{
+    Constants,
+    Field,
+    FieldsHandler,
+    TreeHandler
+};
 
 defined('XOOPS_ROOT_PATH') || die('Restricted access');
 
@@ -32,23 +36,28 @@ defined('XOOPS_ROOT_PATH') || die('Restricted access');
 class Animal
 {
     protected $myTree = [];
-    protected $fields = [];
-    protected $configValues = [];
+    protected $fields = [];       // array of ids for fields
+    protected $configValues = []; // id indexed array of field info
+    private   $helper;            // module Helper object
+    private   $treeHandler;       // Pedigree Tree handler object
 
     /**
-     * class constructor
+     * Animal class constructor
      *
      * initializes the tree array
      * @param int|null $id
      */
-    public function __construct($id = null)
+    public function __construct(?int $id = null)
     {
-        $id = null !== $id ? (int)$id : Constants::DEFAULT_TREE_ID;
-        /** @var XoopsModules\Pedigree\TreeHandler $treeHandler */
-        $treeHandler = XoopsModules\Pedigree\Helper::getInstance()->getHandler('Tree');
-        $criteria = new \Criteria('id', $id);
-        $criteria->setLimit(1);
-        $this->myTree = $treeHandler->getAll($criteria, null, false);
+        $id = !empty($id) ? (int) $id : Constants::DEFAULT_TREE_ID;
+
+        /**
+         * @var Helper $this->helper
+         * @var TreeHandler $this->treeHandler;
+         */
+        $this->helper      = Helper::getInstance();
+        $this->treeHandler = $this->helper->getHandler('Tree');
+        $this->myTree      = $this->treeHandler->get($id);
         /*
         $SQL = "SELECT * FROM " . $GLOBALS['xoopsDB']->prefix("pedigree_tree") . " WHERE id = {$id}";
         $result    = $GLOBALS['xoopsDB']->query($SQL);
@@ -62,22 +71,19 @@ class Animal
     }
 
     /**
-     * Ids of Fields
+     * Get Ids of Fields
+     *
      * @return array
      */
-    public function getFieldsIds()
+    public function getFieldsIds(): array
     {
-        /** @var XoopsModules\Pedigree\FieldsHandler $fieldsHandler */
-        $fieldsHandler = XoopsModules\Pedigree\Helper::getInstance()->getHandler('Fields');
-        $criteria = new \Criteria();
+        /** @var FieldsHandler $fieldsHandler */
+        $fieldsHandler = $this->helper->getHandler('Fields');
+        $criteria = new \Criteria('');
         $criteria->setSort('order');
         $criteria->order = 'ASC';
         //$this->fields = $fieldsHandler->getIds($criteria); //get all object IDs
-        $this->configValues = $fieldsHandler->getAll($criteria, null, false, true); //get objects as arrays w/ id as array key
-        if (empty($this->configValues)) {
-            /** @internal changed from '' to [] in v1.32 Alpha 1; not sure this is really needed since getAll() above will return empty array */
-            $this->configValues = [];
-        }
+        $this->configValues = $fieldsHandler->getAll($criteria, null, false, true); //get array of fields w/ id as array key
         $this->fields = array_keys($this->configValues);
         /*
         $SQL    = "SELECT * FROM " . $GLOBALS['xoopsDB']->prefix("pedigree_fields") . " ORDER BY `order`";
@@ -97,9 +103,11 @@ class Animal
     }
 
     /**
+     * Get indexed array of field configs
+     *
      * @return array
      */
-    public function getConfig()
+    public function getConfig(): array
     {
         return $this->configValues;
     }
@@ -114,15 +122,15 @@ class Animal
      *
      * @return array form element array to be added to form
      */
-    public function getFormFieldsElements()
+    public function getFormFieldsElements(): array
     {
         $formElements = [];
         $this->fields = $this->getFieldsIds();
         $fieldCount = count($this->fields);
         for ($i = 0; $i < $fieldCount; ++$i) {
-            $userField = new Pedigree\Field($this->fields[$i], $this->getConfig());
+            $userField = new Field($this->fields[$i], $this->getConfig());
             if ($userField->isActive()) {
-                $fieldType = $userField->getVar('fieldtype');
+                $fieldType = $userField->getSetting('fieldtype');
                 $fieldObject = new $fieldType($userField, $this);
                 $edditable[$i] = $fieldObject->editField();
                 $formElements[] = $edditable[$i];
